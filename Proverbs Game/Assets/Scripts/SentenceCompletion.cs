c7f7943b0ed1807c636f0d85d16dc0ef5a6eb2d3
ferhanyildiz <f.yildiz@student.tudelft.nl>
2022-05-10 13:38:51 +0200

    Added functionality for inputting and removing words

--- Proverbs Game/Assets/Scripts/SentenceCompletion.cs
+++ Proverbs Game/Assets/Scripts/SentenceCompletion.cs
@@ -13,17 +13,62 @@ public class SentenceCompletion : MonoBehaviour
     private string answerProverb = correctProverb;
 
     List<string> keyWords = new List<string> { "horse", "mouth" };
+    List<string> allWords = new List<string> { "horse", "mouth", "turtle", "Never", "Don't"};
+    public string LastClickedWord;
 
-// Start is called before the first frame update
-void Start()
+    // Start is called before the first frame update
+    void Start()
     {
         foreach (string v in keyWords)
         {
             answerProverb = answerProverb.Replace(v, "...");
         }
 
+
+
         sentence.text = answerProverb;
     }
 
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            var wordIndex = TMP_TextUtilities.FindIntersectingWord(sentence, Input.mousePosition, null);
+
+            if (wordIndex != -1)
+            {
+                LastClickedWord = sentence.textInfo.wordInfo[wordIndex].GetWord();
+
+                if (allWords.Contains(LastClickedWord))
+                {
+                    removeWord(LastClickedWord);
+                }
+            }
+        }
+    }
+
+    private void inputWord(string word)
+    {
+        word = "<u><b>" + word + "</u></b>";
+        answerProverb = ReplaceFirst(answerProverb, "...", word);
+        sentence.text = answerProverb;
+    }
+
+    private void removeWord(string word)
+    {
+        word = "<u><b>" + word + "</u></b>";
+        answerProverb = ReplaceFirst(answerProverb, word, "...");
+        sentence.text = answerProverb;
+    }
+
+    public string ReplaceFirst(string text, string search, string replace)
+    {
+        int pos = text.IndexOf(search);
+        if (pos < 0)
+        {
+            return text;
+        }
+        return text.Substring(0, pos) + replace + text.Substring(pos + search.Length);
+    }
 
 }
