5aee296aec593beb78bdc1386a7df9dea24a99d2
Elvira <E.Voorneveld@student.tudelft.nl>
2022-05-17 14:27:01 +0200

    Create basic session manager
    Uses the email to lookup the key of a player, which is then used to find that player's proficiencies

--- Assets/Scripts/DatabaseManager.cs
+++ Assets/Scripts/DatabaseManager.cs
@@ -133,21 +133,22 @@ public class DatabaseManager : MonoBehaviour
     // Start a coroutine for each bit of data to make sure everything is loaded
     public void GetProverbInfo()
     {
+        // Coroutine for retrieving the proverb
         StartCoroutine(GetPhrase((string proverb) => 
         {
             ProverbText.text = "Proverb: " + proverb;
         }));
-
+        // Coroutine for retrieving the keywords
         StartCoroutine(GetKeywords((string keywords) => 
         {
             KeywordsText.text = "Keywords: " + keywords;
         }));
-
+        // Coroutine for retrieving the meaning
         StartCoroutine(GetMeaning((string meaning) => 
         {
             MeaningText.text = "Meaning: " + meaning;
         }));
-
+        // Coroutine for retrieving the example
         StartCoroutine(GetExample((string example) => 
         {
             ExampleText.text = "Example: " + example;
---
+++ Assets/Scripts/SessionManager.cs
@@ -0,0 +1,73 @@
+using Firebase;
+using Firebase.Database;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SessionManager : MonoBehaviour
+{
+    // TextMeshPro input fields
+    public TMP_InputField PlayerEmail;
+
+    // Stores the reference location of the database
+    private DatabaseReference dbReference;
+
+    // Stores the player key
+    private string playerKey;
+    private Proficiency playerProficiency;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Get the root reference location of the database
+        dbReference = FirebaseDatabase.DefaultInstance.RootReference;
+    }
+
+    public void GetPlayerKey()
+    {
+        dbReference.Child("players").OrderByChild("email").EqualTo(PlayerEmail.text)
+        .ValueChanged += (object sender, ValueChangedEventArgs args) =>
+        {
+            if (args.DatabaseError != null)
+            {
+                Debug.LogError(args.DatabaseError.Message);
+                return;
+            }
+
+            if (args.Snapshot != null && args.Snapshot.ChildrenCount > 0)
+            {
+                foreach (var childSnapshot in args.Snapshot.Children)
+                {
+                    playerKey = childSnapshot.Key;
+                    Debug.Log(childSnapshot.Key);
+                    GetPlayerProficiencies();
+                }
+            }
+        };
+    }
+
+    public void GetPlayerProficiencies()
+    {
+        dbReference.Child("proficiencies").Child(playerKey)
+        .GetValueAsync().ContinueWith(task =>
+        {
+            if  (task.IsFaulted)
+            {
+                Debug.LogError("Task could not be completed.");
+                return;
+            }
+            
+            else if (task.IsCompleted)
+            {
+                DataSnapshot snapshot = task.Result;
+                string json = snapshot.GetRawJsonValue();
+                Proficiency playerProficiency = JsonUtility.FromJson<Proficiency>(json);
+                Debug.Log(json);
+            }
+        });
+    }
+}
