958ea79cb6e1b4ec745a1da3183b1100d0536240
Vlad Iftimescu <V.G.Iftimescu@student.tudelft.nl>
2022-05-24 14:26:53 +0200

    Add basic Photon connections + multiplayer menus
    Added the basic Photon skeleton for creating and joining rooms. In the process, also created the menus for the multiplayer section, together with its functionalities.

---
+++ Assets/Scripts/Launcher.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using TMPro;
+
+public class Launcher : MonoBehaviourPunCallbacks {
+    [SerializeField] TMP_InputField roomNameInputField;
+    [SerializeField] TMP_Text errorText;
+    [SerializeField] TMP_Text roomNameText;
+
+    void Start() {
+        Debug.Log("Connecting to Master.");
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster() {
+        Debug.Log("Connected to Master.");
+        PhotonNetwork.JoinLobby(); // you need to be in a Lobby to join a Room
+    }
+
+    public override void OnJoinedLobby() {
+        MenuManager.Instance.OpenMenu("Title"); // open the title menu on joining lobby
+        Debug.Log("Joined Lobby.");
+    }
+
+    public void CreateRoom() {
+        if (string.IsNullOrEmpty(roomNameInputField.text))
+            return;
+
+        PhotonNetwork.CreateRoom(roomNameInputField.text);
+        MenuManager.Instance.OpenMenu("Loading");
+    }
+
+    public override void OnJoinedRoom() {
+        MenuManager.Instance.OpenMenu("Room");
+        roomNameText.text = PhotonNetwork.CurrentRoom.Name;
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string message) {
+        errorText.text = "Room Creation Failed: " + message;
+        MenuManager.Instance.OpenMenu("Error");
+    }
+}
