fea26f1ce67d3e6648d75a8233478636e9c017ee
Dan <D.Savastre@student.tudelft.nl>
2022-06-07 15:23:50 +0200

    Create FunFact scene and script

---
+++ Assets/Scripts/FunFactManager.cs
@@ -0,0 +1,86 @@
+using Firebase;
+using Firebase.Database;
+using Firebase.Storage;
+using Firebase.Extensions;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using Random = UnityEngine.Random;
+
+public class FunFactManager : SingleplayerManager
+{
+
+    [SerializeField] private TextMeshProUGUI funFactText;
+    [SerializeField] private RawImage image;
+    private StorageReference storageRef;
+    private string currentImage;
+    private byte[] fileContents;
+
+    // Start is called before the first frame update.
+    protected async override void Start()
+    {
+        base.Start();
+
+        // Goes to the 'proverbs' database table and searches for the key
+        await dbReference.Child("proverbs").Child(currentBucket.key)
+        .GetValueAsync().ContinueWith(task =>
+        {
+            if (task.IsFaulted)
+            {
+                Debug.LogError("Task (get next proverb) could not be completed.");
+                return;
+            }
+            
+            else if (task.IsCompleted)
+            {
+                // Take a snapshot of the database entry
+                DataSnapshot snapshot = task.Result;
+                // Convert the JSON back to a Proverb object
+                string json = snapshot.GetRawJsonValue();
+                nextProverb = JsonUtility.FromJson<Proverb>(json);
+                Debug.Log(json);
+            }
+        });
+
+        // Get a reference to the storage service, using the default Firebase App
+        storageRef = FirebaseStorage.DefaultInstance.GetReferenceFromUrl("gs://sp-proverb-game.appspot.com");
+
+        // Reference for retrieving an image
+        StorageReference imageRef = storageRef.Child("proverbs/" + nextProverb.image);
+        Debug.Log("proverbs/" + nextProverb.image);
+
+        const long maxAllowedSize = 1 * 1024 * 1024;
+        imageRef.GetBytesAsync(maxAllowedSize).ContinueWithOnMainThread(task =>
+        {
+            if (task.IsFaulted || task.IsCanceled)
+            {
+                Debug.LogError("Task (get image byte array) could not be completed.");
+                return;
+            }
+            
+            else if (task.IsCompleted)
+            {
+                fileContents = task.Result;
+                Texture2D tex = new Texture2D(2, 2);
+                tex.LoadImage(fileContents);
+                image.GetComponent<RawImage>().texture = tex;
+                Debug.Log("Finished downloading!");
+            }
+        });
+        
+        DisplayFunFact();
+    }
+
+    private void DisplayFunFact() 
+    {
+        nextQuestionButton.SetActive(true);
+        Debug.Log(nextProverb.phrase);
+        funFactText.text = nextProverb.phrase;
+    }
+
+}
