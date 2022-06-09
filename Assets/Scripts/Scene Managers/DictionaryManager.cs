012304eb2ebbb34eb950765ddf7200b3402a2bf2
shayan <s.ramezani@student.tudelft.nl>
2022-06-09 17:48:52 +0200

    Fixed the bugs of the DictionaryManager.cs - The filtering is done correctly - The buttons are now added nicely - The words can be "unfiltered"

--- Assets/Scripts/Scene Managers/DictionaryManager.cs
+++ Assets/Scripts/Scene Managers/DictionaryManager.cs
@@ -7,11 +7,12 @@ using Unity.VisualScripting;
 using UnityEditor.IMGUI.Controls;
 using UnityEngine;
 using UnityEngine.UI;
+using WebSocketSharp;
 
 public class DictionaryManager : MonoBehaviour
 {
-    private readonly List<ProverbsDictionary> proverbs;
-    private List<ProverbsDictionary> proverbs_editableList;
+    private readonly List<ProverbsDictionary> allProverbs;
+    private List<ProverbsDictionary> filteredProverbsList;
     private List<string> wordsToFilterOn;
     
     [SerializeField] private TextMeshProUGUI filterText;
@@ -21,9 +22,9 @@ public class DictionaryManager : MonoBehaviour
 
     public DictionaryManager()
     {
-        proverbs = new List<ProverbsDictionary>(new []
+        allProverbs = new List<ProverbsDictionary>(new []
         {
-            new ProverbsDictionary("Don't look a gift hose in the mouth", "meaning"),
+            new ProverbsDictionary("<b>Don't look a gift horse in his mouth</b>", "meaning"),
             new ProverbsDictionary("slow and steady wins the race", "meaning2"),
             new ProverbsDictionary("To be the black sheep of the family", "if you are different, you are oft not accepted in a group."),
             new ProverbsDictionary("His bark is worst than his bite", "To make a lot of noise without reason apparently."),
@@ -33,41 +34,47 @@ public class DictionaryManager : MonoBehaviour
             new ProverbsDictionary("All that glitters is not gold", "said about something that seems to be good on the surface, but might not be when you look at it more closely"),
             new ProverbsDictionary("In the land of the blind, the one-eyed man is king", "Even someone without much talent or ability is considered special by those with no talent or ability at all. ")
         });
-        proverbs_editableList = new List<ProverbsDictionary>(proverbs);
+        filteredProverbsList = new List<ProverbsDictionary>(allProverbs);
         wordsToFilterOn = new List<string>();
+
+        
     }
     
     // Start is called before the first frame update
     void Start()
     {
         UpdateDictionaryContentHolderContents();
+        UpdateDictionaryContentHolderContents();
     }
     
     public void FilterAdded()
     {
-        Debug.Log(filterText.text);
-        if (!filterText.text.Contains(Environment.NewLine) && !filterText.text.Contains(" ") || filterText.text.Equals("")) return;
+        string filter = filterText.text.Replace("\u200B", "");
+        if (filter.Length < 2) return;
+        List<string> currentWordsToFilterOn = filter.ToLower().Trim().Split().ToList();
+        
 
-        string wordToFilterOn = filterText.text;
-        Debug.Log("word to filter on:" + wordToFilterOn);
-        wordsToFilterOn.Add(wordToFilterOn);
-        proverbs_editableList = proverbs_editableList.Where(s => s.proverb.SplitWords(' ').Contains(wordToFilterOn)).ToList();
-        UpdateDictionaryContentHolderContents();
+        foreach (string wordToFilterOn in currentWordsToFilterOn)
+        {
+            wordsToFilterOn.Add(wordToFilterOn);
+            filteredProverbsList = filteredProverbsList.Where(s => s.proverb.ToLower().Split().Contains(wordToFilterOn))
+                .ToList();
 
-        // add button for word filtered on 
-        Button wordButton = Instantiate(wordButtonPrefab, filterTextHolderPanel);
+            // add button for word filtered on 
+            Button wordButton = Instantiate(wordButtonPrefab, filterTextHolderPanel);
             wordButton.GetComponentInChildren<TextMeshProUGUI>().text = wordToFilterOn;
-        wordButton.onClick.AddListener(() => WordButtonPressed(wordsToFilterOn.Count - 1));
-        
+            int x = wordsToFilterOn.Count - 1;
+            wordButton.onClick.AddListener(() => WordButtonPressed(x));
+        }
+        UpdateDictionaryContentHolderContents();
         filterText.text = "";
-        
     }
 
     private void UpdateDictionaryContentHolderContents()
     {
-        proverbs_editableList = proverbs_editableList.OrderBy(p => p.proverb).ToList();
+        filteredProverbsList = filteredProverbsList.OrderBy(p => p.proverb).ToList();
         dictionaryContentHolder.text = "";
-        foreach (var proverbsDictionary in proverbs_editableList)
+        foreach (var proverbsDictionary in filteredProverbsList)
         {
             dictionaryContentHolder.text += proverbsDictionary.proverb + ":" + Environment.NewLine + "  " +
                                             proverbsDictionary.meaning + Environment.NewLine + Environment.NewLine;
@@ -76,10 +83,14 @@ public class DictionaryManager : MonoBehaviour
 
     private void WordButtonPressed(int wordIndex)
     {
-        string wordOnWhichToPutProverbsBackOn = wordsToFilterOn[wordIndex];
-        wordsToFilterOn.RemoveAt(wordIndex);    
-        Destroy(filterTextHolderPanel.GetComponents<Button>()[wordIndex]);
-        proverbs_editableList.AddRange(proverbs.Where(s => s.proverb.Contains(wordOnWhichToPutProverbsBackOn)).ToList());
+        wordsToFilterOn.RemoveAt(wordIndex);
+        Destroy(filterTextHolderPanel.GetChild(wordIndex).GameObject());
+        
+        filteredProverbsList = new List<ProverbsDictionary>(allProverbs);
+        foreach (var wordToFilterOn in wordsToFilterOn)
+        {
+            filteredProverbsList = filteredProverbsList.Where(s => s.proverb.ToLower().Contains(wordToFilterOn)).ToList();
+        }
         UpdateDictionaryContentHolderContents();
     }
     
