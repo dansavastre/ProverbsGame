c535d9ea5e7a79b3fed524be51ba7f1f07f4fd2e
Dan <D.Savastre@student.tudelft.nl>
2022-06-16 00:43:11 +0200

    Add script to upload proverbs to the db

--- Assets/Scripts/ExcelConverter.cs
+++ Assets/Scripts/ExcelConverter.cs
@@ -5,6 +5,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Text.RegularExpressions;
+using UnityEditor;
+using UnityEngine.Networking;
+using TMPro;
 
 public class ExcelConverter : MonoBehaviour
 {
@@ -12,28 +15,42 @@ public class ExcelConverter : MonoBehaviour
     // Stores the reference location of the database
     private DatabaseReference dbReference;
 
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    public TextMeshProUGUI resultText;
+
+    // void Start()
+    // {
+    //     parseList("We must be careful with horses.,When we receive a gift, we must show happiness, even if it is not to our liking.,We should always show satisfaction with a gift, if it is to our liking.");
+    // }
+
+    public void UploadProverbs()
     {
         // Get the root reference location of the database
         dbReference = FirebaseDatabase.DefaultInstance.RootReference;
 
         // Fetch the .csv file from the Resources folder
         TextAsset proverbsCSV = Resources.Load<TextAsset>("Proverbs");
+        
+        // check if the file exists
+        if(proverbsCSV == null)
+        {
+            resultText.text = "The file could not be loaded. Make sure the file name is \"proverbs.csv\" and that the file is in the Resources folder";
+        }
+        
         string[] data = proverbsCSV.text.Split(new char[] { '\n' });
 
-        for(int i = 1; i < 3/*data.Length - 1*/; i++)
+        for(int i = 1; i < data.Length - 1; i++)
         {
-            Regex regx = new Regex(',' + "(?=(?:[^\"]*\"[^\"]*\")*(?![^\"]*\"))"); 
+            Regex regx = new Regex(',' + "(?=(?:[^\"]*\"[^\"]*\")*(?![^\"]*\"))");
             string[] row = regx.Split(data[i]);
-
             List<string> keywords = parseList(row[3].Substring(1, row[3].Length - 2));
             List<string> otherKeywords = parseList(row[4].Substring(1, row[4].Length - 2));
             List<string> otherPhrases = parseList(row[2].Substring(1, row[2].Length - 2));
             List<string> otherMeanings = parseList(row[6].Substring(1, row[6].Length - 2));
             List<string> otherExamples = parseList(row[8].Substring(1, row[8].Length - 2));
 
-            Proverb proverb = new Proverb(row[1], keywords, row[5], row[7], row[10], otherPhrases, otherMeanings, otherExamples);
+            Proverb proverb = new Proverb(  row[1], keywords, row[5].Substring(1, row[5].Length - 2), row[7].Substring(1, row[7].Length - 2).Replace("\"\"", "\""), 
+                                            row[10].Replace("\r", ""), otherPhrases, otherKeywords, otherMeanings, otherExamples, row[9].Substring(1, row[9].Length - 2));
             Debug.Log(JsonUtility.ToJson(proverb));
 
             // Add the proverb to the database
@@ -45,11 +62,12 @@ public class ExcelConverter : MonoBehaviour
     private List<string> parseList(string wordsList)
     {
         List<string> res = new List<string>();
-        string[] words = wordsList.Split(new char[] { ',' });
+
+        Regex regx = new Regex(@",(?=\S)");
+        string[] words = regx.Split(wordsList);
         foreach(var word in words)
         {
-            res.Add(word);
-            //Debug.Log(str);
+            res.Add(word.Replace("\"\"", "\""));
         }
         return res;
     }
