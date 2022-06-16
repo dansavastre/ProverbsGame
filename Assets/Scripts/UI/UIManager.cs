eec0d9c2799fdf8cff9c32f075c963416320a080
Elvira <E.Voorneveld@student.tudelft.nl>
2022-06-16 13:07:45 +0200

    Add prefab for overlay for level up of proverb proficiency

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -6,17 +6,39 @@ using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
-    public AudioSource WoodButton;
+    [SerializeField] public AudioSource WoodButton;
+    [SerializeField] private GameObject ProverbLevelUp;
+    private ParticleSystem ConfettiPS;
+    private TextMeshProUGUI ProficiencyText;
 
     // Start is called before the first frame update
     void Awake()
     {
         // GameObject[] buttonAudio = GameObject.FindGameObjectsWithTag("WoodButtonAudio");
-        DontDestroyOnLoad(WoodButton);
+        if (WoodButton != null) DontDestroyOnLoad(WoodButton);
+        ConfettiPS = GameObject.Find("Confetti").GetComponent<ParticleSystem>();
+        ProficiencyText = GameObject.Find("Proficiency").GetComponent<TextMeshProUGUI>();
+        ProverbLevelUp.SetActive(false);
+        StartCoroutine(enableCongratulations());
     }
 
     public void onMouseClick() 
     {
         WoodButton.Play();
+    }
+
+    IEnumerator enableCongratulations()
+    {
+        yield return new WaitForSeconds(2);
+        ProverbLevelUp.SetActive(true);
+        ConfettiPS.Play();
+        yield return new WaitForSeconds(5);
+        disableCongratulations();
+    }
+
+    public void disableCongratulations()
+    {
+        ProverbLevelUp.SetActive(false);
+        ConfettiPS.Stop();
     }
 }
